ed8a76c345b43618a936f7ce0511a39c82f831cd
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2025-01-07 12:58:07 +0100

    STM32: Implement pulse on compare mode for timer_v2

--- embassy-stm32/src/timer/low_level.rs
+++ embassy-stm32/src/timer/low_level.rs
@@ -369,6 +369,39 @@ fn div_round(numerator: u64, denominator: u64, round: RoundTo) -> u64 {
     }
 }
 
+/// Pulse width prescaler.
+#[cfg(timer_v2)]
+#[allow(missing_docs)]
+#[derive(Clone, Copy)]
+#[repr(u8)]
+pub enum PulseWidthPrescaler {
+    Div1 = 0,
+    Div2 = 1,
+    Div4 = 2,
+    Div8 = 3,
+    Div16 = 4,
+    Div32 = 5,
+    Div64 = 6,
+    Div128 = 7
+}
+
+#[cfg(timer_v2)]
+impl From<u8> for PulseWidthPrescaler {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => PulseWidthPrescaler::Div1,
+            1 => PulseWidthPrescaler::Div2,
+            2 => PulseWidthPrescaler::Div4,
+            3 => PulseWidthPrescaler::Div8,
+            4 => PulseWidthPrescaler::Div16,
+            5 => PulseWidthPrescaler::Div32,
+            6 => PulseWidthPrescaler::Div64,
+            7 => PulseWidthPrescaler::Div128,
+            _ => unreachable!()
+        }
+    }
+}
+
 /// Low-level timer driver.
 pub struct Timer<'d, T: CoreInstance> {
     tim: Peri<'d, T>,
@@ -1075,6 +1108,30 @@ impl<'d, T: GeneralInstance4Channel> Timer<'d, T> {
     /// Set Timer External Clock Mode 2 Enable state
     pub fn set_external_clock_mode_2_enable_state(&self, val: bool) {
         self.regs_gp16().smcr().modify(|w| w.set_ece(val));
+    }
+
+    /// Get the pulse width of the generated pulses in pulse on compare mode
+    #[cfg(timer_v2)]
+    pub fn get_pulse_width(&self) -> u8 {
+        self.regs_gp16().ecr().read().pw()
+    }
+
+    /// Set the pulse width of the generated pulses in pulse on compare mode
+    #[cfg(timer_v2)]
+    pub fn set_pulse_width(&self, pw: u8) {
+        self.regs_gp16().ecr().modify(|r| r.set_pw(pw));
+    }
+
+    /// Get the prescaler of the pulse generator for pulse on compare mode
+    #[cfg(timer_v2)]
+    pub fn get_pulse_width_prescaler(&self) -> PulseWidthPrescaler {
+        self.regs_gp16().ecr().read().pwprsc().into()
+    }
+   
+    /// Set the prescaler of the pulse generator for pulse on compare mode
+    #[cfg(timer_v2)]
+    pub fn set_pulse_width_prescaler(&self, prsc: PulseWidthPrescaler) {
+        self.regs_gp16().ecr().modify(|r| r.set_pwprsc(prsc as u8));
     }
 }
 
